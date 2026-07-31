efd9c3ac751dba54b5629626ebc4dc0a8ce1ce7a
pranay8873 <pranaypuvaati@gmail.com>
2026-08-01 00:34:45 +0530

    fead: lawyer model is defined

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,String,Integer,Boolean
+from sqlalchemy import Column,String,Integer,Boolean, column
 from database import Base
 
 
@@ -17,6 +17,16 @@ class Police(Base):
     age=Column(Integer)
     gender=Column(String(10))
     role=Column(String(100))
+
+class Lawyer(Base):
+    __tablename__="lawyers"
+    id=Column(Integer,primary_key=True,index=True)
+    name=Column(String(100))
+    age=Column(Integer)
+    gender=Column(String(50))
+    role=Column(String(50))
+
+    
 
 
 
